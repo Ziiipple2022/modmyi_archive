e76d2d9c5217214a7f79d3887efdbac90c8da7bf
NostalGiOS Project <68229596+Ziiipple2022@users.noreply.github.com>
2023-12-11 21:57:22 +0100

    Add files via upload

---
+++ modmyi_archive/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './View';
+
+function App() {
+  return (
+    <MyComponent />
+  );
+}
+
+export default App;
